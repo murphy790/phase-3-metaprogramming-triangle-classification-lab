05746b862ce3b5fc4a9e724f090219a6ae77f194
Alex Mwaura <alexmwaura36@gmail.com>
2022-08-21 15:05:53 +0300

    lab complete

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_reader :x, :y, :z
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+    triangle_validation
+    if x == y && y == z
+      :equilateral
+    elsif x == y || y == z || x == z
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def triangle_validation
+    real_triangle = [(x + y > z), (x + z > y), (y + z > x)]
+    [x, y, z].each { |s| real_triangle << false if s <= 0 }
+    raise TriangleError if real_triangle.include?(false)
+  end
+
+  class TriangleError < StandardError
+    def message
+      "invalid triangle"
+    end
+  end
+
+
 end
